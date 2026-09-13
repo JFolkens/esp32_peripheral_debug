dbb9b3bf112792cdfbca164030d1ddd59cec08d3
JFolkens <jfolkens.oh@gmail.com>
2026-09-13 11:40:31 -0400

    Got the CMakeLists asset header generation working

--- main/cmake/GenerateAssetHeader.cmake
+++ main/cmake/GenerateAssetHeader.cmake
@@ -1,4 +1,14 @@
-function(generate_asset_header, ASSET_FILES, GENERATED_ASSETS_H)
+function(generate_asset_header)
+    # Parse multi value arg for ASSET_FILES
+    set(multi_value_args ASSET_FILES)
+    cmake_parse_arguments(PARSE_ARGV 0 ARG "" "" "${multi_value_args}")
+
+    set(GENERATED_ASSETS_H "${CMAKE_CURRENT_LIST_DIR}/esp32/assets_esp32.h")
+
+    # Check asset files are found by CMAKE
+    # IMPORTANT: File names must be unique, not just paths.
+    message(STATUS "ASSET_FILES = ${ARG_ASSET_FILES}")
+
     #[[
     If your assets are printed by CMAKE but not being linked, try:
 
@@ -13,7 +23,8 @@ function(generate_asset_header, ASSET_FILES, GENERATED_ASSETS_H)
     set(EMBEDDED_ASSETS_BLOCK "")
 
     # Generate source code blocks
-    foreach(ASSET_FILE ${ASSET_FILES})
+    foreach(ASSET_FILE ${ARG_ASSET_FILES})
+        message(STATUS "Parsing asset: ${ASSET_FILE}")
         get_filename_component(ASSET_NAME "${ASSET_FILE}" NAME)
         string(REGEX REPLACE "[^A-Za-z0-9_]" "_" SYMBOL_BASE "${ASSET_NAME}")
 
@@ -28,21 +39,23 @@ extern const unsigned char _binary_${SYMBOL_BASE}_end[];
     endforeach()
 
     # Update header file
-    file(READ GENERATED_ASSETS_H ASSETS_TEMPLATE)
-    string(REPLACE
-          "/* <----- EXTERN DECLARATION BLOCK -----> */"
-          "${EXTERN_DECLARATION_BLOCK}"
+    file(READ "${GENERATED_ASSETS_H}" ASSETS_TEMPLATE)
+    string(REGEX REPLACE
+          "<----- BEGIN EXTERN DECLARATION BLOCK ----->.*// <----- END EXTERN DECLARATION BLOCK ----->"
+          "<----- BEGIN EXTERN DECLARATION BLOCK ----->
+${EXTERN_DECLARATION_BLOCK}// <----- END EXTERN DECLARATION BLOCK ----->"
           ASSETS_WITH_EXTERN
-          ASSETS_TEMPLATE
+          "${ASSETS_TEMPLATE}"
     )
 
-    string(REPLACE
-           "/* <------ EMBEDDED ASSETS BLOCK -----> */"
-           "${EMBEDDED_ASSETS_BLOCK}"
+    string(REGEX REPLACE
+          "<----- BEGIN EMBEDDED ASSETS BLOCK ----->.*// <----- END EMBEDDED ASSETS BLOCK ----->"
+          "<----- BEGIN EMBEDDED ASSETS BLOCK ----->
+${EMBEDDED_ASSETS_BLOCK}    // <----- END EMBEDDED ASSETS BLOCK ----->"
            ASSETS_UPDATED
-           ASSETS_WITH_EXTERN
+           "${ASSETS_WITH_EXTERN}"
     )
 
-    file(WRITE GENERATED_ASSETS_H "${ASSETS_UPDATED}")
+    file(WRITE "${GENERATED_ASSETS_H}" "${ASSETS_UPDATED}")
 
 endfunction()
